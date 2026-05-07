3c90bded9d6630cdc8606c17bdbcea134fed8405
Zeke <ezequielgerstel777@gmail.com>
2026-05-06 23:45:41 -0400

    Tablas ComputerStorage

---
+++ ComputerStorageOLTP/Tables/orders.sql
@@ -0,0 +1,26 @@
+﻿CREATE TABLE orders
+(
+	[order_id]      [int] IDENTITY(1,1) NOT NULL CONSTRAINT PK_orders PRIMARY KEY,
+	[customer_id]   [int]               NULL,
+	[order_status]  [tinyint]           NOT NULL,
+	[order_date]    [date]              NOT NULL,
+	[required_date] [date]              NOT NULL,
+	[shipped_date]  [date]              NULL,
+	[store_id]      [int]               NOT NULL,
+	[staff_id]      [int]               NOT NULL,
+	[rowversion]    [timestamp]         NOT NULL
+);
+GO
+
+ALTER TABLE orders ADD CONSTRAINT FK_orders_customers FOREIGN KEY([customer_id]) REFERENCES customers ([customer_id])
+ON UPDATE CASCADE
+ON DELETE CASCADE
+GO
+
+ALTER TABLE orders ADD CONSTRAINT FK_orders_staffs FOREIGN KEY([staff_id]) REFERENCES staffs ([staff_id])
+GO
+
+ALTER TABLE orders ADD CONSTRAINT FK_orders_stores FOREIGN KEY([store_id]) REFERENCES stores ([store_id])
+ON UPDATE CASCADE
+ON DELETE CASCADE
+GO
---
+++ ComputerStorageOLTP/Tables/stocks.sql
@@ -0,0 +1,21 @@
+﻿CREATE TABLE stocks
+(
+	[store_id]   [int]       NOT NULL,
+	[product_id] [int]       NOT NULL,
+	[quantity]   [int]       NULL,
+	[rowversion] [timestamp] NOT NULL
+);
+GO
+
+ALTER TABLE stocks ADD CONSTRAINT PK_stocks PRIMARY KEY(store_id, product_id);
+GO
+
+ALTER TABLE stocks ADD CONSTRAINT FK_stocks_products  FOREIGN KEY([product_id]) REFERENCES products ([product_id])
+ON UPDATE CASCADE
+ON DELETE CASCADE
+GO
+
+ALTER TABLE stocks ADD CONSTRAINT FK_stocks_stores FOREIGN KEY([store_id]) REFERENCES stores ([store_id])
+ON UPDATE CASCADE
+ON DELETE CASCADE
+GO
