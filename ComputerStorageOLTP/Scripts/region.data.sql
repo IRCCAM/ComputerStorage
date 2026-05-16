8b39f93e7e422a3c8a5c71c39ea58b279675a406
IRCCAM <reneccama2@gmail.com>
2026-05-16 04:49:46 -0400

    Fix script data

--- ComputerStorageOLTP/Scripts/region.data.sql
+++ ComputerStorageOLTP/Scripts/region.data.sql
@@ -1,8 +1,8 @@
 IF NOT EXISTS(SELECT TOP(1) 1 FROM [production].[region])
 BEGIN
-    INSERT [production].[region] ([RegionID], [RegionDescription]) VALUES (1, N'Eastern')
-    INSERT [production].[region] ([RegionID], [RegionDescription]) VALUES (2, N'Western')
-    INSERT [production].[region] ([RegionID], [RegionDescription]) VALUES (3, N'Northern')
-    INSERT [production].[region] ([RegionID], [RegionDescription]) VALUES (4, N'Southern')
+    INSERT [production].[region] ([RegionID], [RegionDescription]) VALUES (1, N'Eastern                                           ')
+    INSERT [production].[region] ([RegionID], [RegionDescription]) VALUES (2, N'Western                                           ')
+    INSERT [production].[region] ([RegionID], [RegionDescription]) VALUES (3, N'Northern                                          ')
+    INSERT [production].[region] ([RegionID], [RegionDescription]) VALUES (4, N'Southern                                          ')
 END
 GO
